e83b1feed0ffe721faee61ed93cd48321d1f863c
Phiy-O <andikapiyo12@gmail.com>
2025-06-29 07:09:17 +0700

    Create score.py

---
+++ 7 Kyu/Python/score.py
@@ -0,0 +1,17 @@
+def scoreboard(st):
+    score = {'nil':0, 
+             'one':1, 
+             'two':2, 
+             'three':3, 
+             'four':4,
+             'five':5,
+             'six':6,
+             'seven':7,
+             'eight':8,
+             'nine':9}
+    arr = []
+    s = st.split()
+    for x in s:
+        if x in score:
+            arr.append(score[x])
+    return arr
