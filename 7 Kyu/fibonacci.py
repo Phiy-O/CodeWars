05ea475530b88967aef6141a7b138164ec9e446e
Phiy-O <andikapiyo12@gmail.com>
2025-07-29 06:40:51 +0700

    Create fibonacci.py

---
+++ 7 Kyu/fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n: int) -> int:
+    """Given a positive argument n, returns the nth term of the Fibonacci Sequence.
+    """
+    arr = [0 , 1]
+    for x in range(1, n):
+        arr.append(arr[-1] + arr[-2])
+    if n == 0:
+        return 0
+    return arr[len(arr) - 1]
